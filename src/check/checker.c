fa91121885cc8265d5d82a59e9b1c7d11b5a5778
Tommy <tommy@localhost.localdomain>
2021-12-02 17:51:25 +0100

    new map

--- src/check/checker.c
+++ src/check/checker.c
@@ -78,13 +78,41 @@ static void	parse_in_colors(char **map)
 	}			
 }
 
-static void	copy_to_new_map(char **new_map, t_map *mapi)
+static void	copy_old_in_new_map(char **new_map, t_map *mapi)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	i = 0;
+	k = 1;
+	while (mapi->map[i])
+	{
+		j = 0;
+		l = 1;
+		while (mapi->map[i][j] != '\0')
+		{
+			if (mapi->map[i][j] != ' ')
+				new_map[k][l] = mapi->map[i][j];
+			j++;
+			l++;
+			printf("**%s**\n", mapi->map[i]);
+		}
+		i++;
+		k++;
+	}
+	ft_free_dp(mapi->map);
+	mapi->map = new_map;
+}
+
+static void	write_x_in_new_map(char **new_map, t_map *mapi)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	while(new_map[i] != NULL)
+	while(new_map[i])
 	{
 		j = 0;
 		while(new_map[i][j])
@@ -93,24 +121,8 @@ static void	copy_to_new_map(char **new_map, t_map *mapi)
 			j++;
 		}
 		i++;
-		printf("**%d - %d - %s**\n", i, j, new_map[i]);
 	}
-	i = 0;
-	while (mapi->map[i])
-	{
-		j = 0;
-		while (mapi->map[i][j])
-		{
-			if (mapi->map[i][j] != ' ')
-			{
-				new_map[i + 1][j + 1] = mapi->map[i][j];
-				j++;
-			}
-			i++;
-		}
-	}
-	ft_free_dp(mapi->map);
-	mapi->map = new_map;
+	copy_old_in_new_map(new_map, mapi);
 }
 
 static void	redimension_map(t_map *mapi, int i, int size_line)
@@ -120,15 +132,15 @@ static void	redimension_map(t_map *mapi, int i, int size_line)
 	i = i + 3;
 	size_line = size_line + 3;
 	new_map = (char **) malloc(sizeof(char *) * i);
-	printf("***%d***\n", i - 1);
 	new_map[i - 1] = NULL;
-	while (i > 0)
+	i = i - 2;
+	while (i >= 0)
 	{
-		new_map[i - 1] = (char *) malloc(sizeof(char) * size_line);
-		new_map[i - 1][size_line - 1] = '\0';
+		new_map[i] = (char *) malloc(sizeof(char) * size_line);
+		new_map[i][size_line - 1] = '\0';
 		i--;
 	}
-	copy_to_new_map(new_map, mapi);
+	write_x_in_new_map(new_map, mapi);
 }
 
 static void	parse_in_map(t_map *mapi)
